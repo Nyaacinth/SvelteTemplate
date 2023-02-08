815fdacd1b553dcf1af7cfbbaf532b618a4a1279
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2023-02-08 21:44:43 +0800

    喵叫初始化

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { svelte as Svelte } from "@sveltejs/vite-plugin-svelte"
+import UnoCSS from "unocss/vite"
+
+import { defineConfig } from "vite"
+
+export default defineConfig({
+    server: {
+        port: 1420,
+        strictPort: true
+    },
+    clearScreen: false,
+    envPrefix: ["VITE_", "TAURI_"],
+    build: {
+        target: ["es2021", "chrome105", "safari13"]
+    },
+    plugins: [Svelte(), UnoCSS()]
+})
